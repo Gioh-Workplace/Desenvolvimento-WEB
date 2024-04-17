a263d00a0923384f9c6ae1d6536a01b9bb3944af
Giovani Augusto Justo <giovanijusto54@gmail.com>
2024-04-16 21:47:28 -0300

    RequestParameters,PathVariable e Model.AddObject

--- src/main/java/br/senac/tadsD/sesh/exemplosspringmvc/controller/ExemploController.java
+++ src/main/java/br/senac/tadsD/sesh/exemplosspringmvc/controller/ExemploController.java
@@ -1,10 +1,13 @@
 
 package br.senac.tadsD.sesh.exemplosspringmvc.controller;
 
+import java.time.LocalDateTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -19,8 +22,29 @@ public class ExemploController {
     public ModelAndView saudar2(){
         return new ModelAndView("message-view");
     }
-     @GetMapping("/mensagem3")
-    public ModelAndView saudar3(){
-        return new ModelAndView("message-view");
+     @GetMapping("/exemplo-request-param")
+    public ModelAndView exemploRequestParam(
+            @RequestParam(value="param1str",required = true, defaultValue ="valor") String param1,
+            @RequestParam(value="param2int",required = false, defaultValue ="99") int param2 ){
+        ModelAndView resposta = new ModelAndView("view-exemplo");
+        return resposta;
     }
+    
+    @GetMapping("/exemplo-path-var/{param1str}/{param2int}")
+    public ModelAndView exemploPathVariable (
+            @PathVariable(value="param1str",required = true) String param1,
+            @PathVariable(value="param2int",required = true) int param2) {
+     ModelAndView resposta = new ModelAndView("view-exemplo");
+        return resposta;
+}
+    @GetMapping("/exemplo-view-obj")
+    public ModelAndView exemploViewObj(){
+        ModelAndView mv = new ModelAndView("exemplo-view");
+        mv.addObject("texto","Albion Online é um MMORPG sandbox");
+        mv.addObject("numero",100);
+        mv.addObject("dataHora",LocalDateTime.now());
+        return mv;
+    }
+    
+    
 }
